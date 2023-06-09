d1d0a22521cb922442c98cf86630a1a1c3319b9c
Aleksandar Grigorov <aleksandar_grigorov12@abv.bg>
2023-06-09 21:45:22 +0300

    2

--- rzk-dobrich/src/App.js
+++ rzk-dobrich/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Homepage from './Pages/homepage';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Homepage/>
+  },
+  
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>RZK dobrich</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ rzk-dobrich/src/Pages/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  Drawer  from '../components/drawer'
+
+export default function homepage() {
+  return (
+    <div>homepage
+    <Drawer></Drawer>
+    </div>
+  )
+}
